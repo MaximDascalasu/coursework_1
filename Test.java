7086e0caac045b1898dee84614b5696cf7fdca40
Maxim_Dascalasu <mdasca200@caledonian.ac.uk>
2019-11-07 20:07:08 +0000

    test file

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maxim
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number ");
+        int num = scanner.nextInt();
+        System.out.println("Hexa "+Integer.toHexString(num));
+    }
+}
