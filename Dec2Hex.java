d07eb8d8493a217853b49ae53992010001c17498
Maxim_Dascalasu <mdasca200@caledonian.ac.uk>
2019-11-07 16:16:20 +0000

    version 2

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -9,12 +9,14 @@ class Dec2Hex
     {          
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem,num;
-	String hexadecimal=""; 
+        //Created Scanner
         Scanner input = new Scanner(System.in);   
+        //Promp the user to enter a decimal integer
         System.out.println("Enter a Decimal number: ");
         num=input.nextInt();
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
+        //Convert decimal to hex
+        String hexadecimal="";
         while(num != 0)
         {
 	    rem=num%16;
