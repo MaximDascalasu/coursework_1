bc836b32e067aa50c2619e760eaf86d14c26e997
MaximDascalasu <32527178+MaximDascalasu@users.noreply.github.com>
2019-11-07 17:10:25 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 class Dec2Hex
 {
 
-    public static int Arg1;
-
     public static void main(String args[])
     {          
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
